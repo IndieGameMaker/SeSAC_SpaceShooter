c7cf4571ea31e06b77ac854d5ec9a8257ff8fb9d
Lee JaeHyun <myleje@gmail.com>
2024-08-09 15:58:17 +0900

    .

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -37,6 +37,16 @@ public class PlayerController : MonoBehaviour
         InputAxis();
         Locomotion();
         Animation();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        // 총알 프리팹을 이용해서 런타임에서 동적을 생성
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
     }
 
     private void Animation()
