9f25fc167db1024609c42ffbe4b347638bd17def
Lee JaeHyun <myleje@gmail.com>
2024-08-09 11:20:52 +0900

    .

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // 전역변수 선언
+    private float v;
+
+
     // 1 호출, 제일 먼저 호출 
     void Start()
     {
@@ -11,6 +15,12 @@ public class PlayerController : MonoBehaviour
     // 매 프레임 마다 호출, 60 FPS, 불규칙한 주기, 랜더링 주기와 동일
     void Update()
     {
+        // 축(Axis) 값을 받아옴. -1.0f ~ 0.0 ~ +1.0f
+        v = Input.GetAxis("Vertical");
+        // 콘솔 뷰에 메시지를 출력하는 메소드 Debug.Log("메시지");
+        Debug.Log(v);
+
+
         // transform.position += new Vector3(0, 0, 0.1f);
         // transform.position += Vector3.forward * 0.1f;
         transform.Translate(Vector3.forward * 0.1f);
