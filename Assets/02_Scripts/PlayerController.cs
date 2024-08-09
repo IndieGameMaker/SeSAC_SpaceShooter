2dc684fa7138852fcdbfce717ed8a69be4f93a39
Lee JaeHyun <myleje@gmail.com>
2024-08-09 11:08:53 +0900

    .

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -11,7 +11,8 @@ public class PlayerController : MonoBehaviour
     // 매 프레임 마다 호출, 60 FPS, 불규칙한 주기, 랜더링 주기와 동일
     void Update()
     {
-        transform.position += new Vector3(0, 0, 0.1f);
+        // transform.position += new Vector3(0, 0, 0.1f);
+        transform.position += Vector3.forward * 0.1f;
     }
 
 }
