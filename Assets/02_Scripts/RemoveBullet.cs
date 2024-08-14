640cd90a4d5fd6362ce1cca1e016feacaff725d1
Lee JaeHyun <myleje@gmail.com>
2024-08-14 11:21:07 +0900

    .

--- Assets/02_Scripts/RemoveBullet.cs
+++ Assets/02_Scripts/RemoveBullet.cs
@@ -2,5 +2,21 @@ using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
+    // 충돌 콜백 함수
+    /*
+        1. 양쪽 다 Collider 갖고 있다.
+        2. 이동하는 게임오브젝트에 Rigidbody 있어야 함.
 
+        # IsTrigger 언체크 경우
+            OnCollisionEnter
+            OnCollisionStay
+            OnCollisionExit
+
+        # IsTrigger 체크 경우
+            OnTriggerEnter
+            OnTriggerStay
+            OnTriggerExit
+    
+
+    */
 }
