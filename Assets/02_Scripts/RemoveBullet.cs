be0ddb2ffa4b8d828babad3871d77f9ec4e6223f
Lee JaeHyun <myleje@gmail.com>
2024-08-14 13:19:55 +0900

    .

--- Assets/02_Scripts/RemoveBullet.cs
+++ Assets/02_Scripts/RemoveBullet.cs
@@ -14,7 +14,7 @@ public class RemoveBullet : MonoBehaviour
             // 충돌 좌표
             Vector3 _point = cp.point;
             // 법선 벡터
-            Vector3 _normal = cp.normal;
+            Vector3 _normal = -cp.normal;
 
             // 법선 벡터가 가리키는 방향의 각도(쿼터니언)를 계산
             Quaternion rot = Quaternion.LookRotation(_normal);
