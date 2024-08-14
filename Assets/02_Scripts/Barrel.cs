374c1854671715a5c10638129086d795153e2e7d
Lee JaeHyun <myleje@gmail.com>
2024-08-14 14:40:17 +0900

    .

--- Assets/02_Scripts/Barrel.cs
+++ Assets/02_Scripts/Barrel.cs
@@ -1,5 +1,5 @@
-using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Barrel : MonoBehaviour
 {
@@ -13,7 +13,27 @@ public class Barrel : MonoBehaviour
             if (hitCount >= 3)
             {
                 // 폭발효과
+                ExpBarrel();
             }
         }
+    }
+
+    /*
+        Random.Range(min, max) 
+
+        # 정수 Integer
+        Random.Range(0, 10) => 0, 1, 2, ..., 9
+
+        # 실수 Float
+        Random.Range(0.0f , 10.0f) => 0.0f ~ 10.0f    
+    */
+
+
+    private void ExpBarrel()
+    {
+        var rb = this.gameObject.AddComponent<Rigidbody>();
+
+
+        rb.AddExplosionForce(1500.0f, transform.position, 10.0f, 1800.0f);
     }
 }
